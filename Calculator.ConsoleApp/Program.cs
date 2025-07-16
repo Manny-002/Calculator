023c351bcaea97a233315ac85166465361dbb3e8
Manny <honza.mencler@gmail.com>
2025-07-16 10:05:27 +0200

    Přidejte soubory projektu.

---
+++ Calculator.ConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿
+Console.WriteLine("Toto je kalkulačka");
+
+
+// vstupy pro kalkulačku
+Console.Write("Zadej prvni cislo:");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Zadej druhe cislo:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+//vypis operaci
+
+//int soucet = a+b;
+//int rozdil = a - b;
+//int nasobeni = a * b;
+//int deleni = a / b; 
+
+
+//horsi zpusob
+
+//Console.WriteLine("Součet je:");
+//Console.WriteLine(soucet);
+//Console.WriteLine("Rozdíl je:");
+//Console.WriteLine(rozdil);
+//Console.WriteLine("Součin je:");
+//Console.WriteLine(nasobeni);
+//Console.WriteLine("Podíl je:"); 
+//Console.WriteLine(deleni);
+
