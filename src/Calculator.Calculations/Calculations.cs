51d4398c0d5a991a8f1e2c11b0fbf2c52a5741d7
Manny <honza.mencler@gmail.com>
2025-07-16 15:19:09 +0200

    using Serilog in class Calculations

--- src/Calculator.Calculations/Calculations.cs
+++ src/Calculator.Calculations/Calculations.cs
@@ -1,12 +1,40 @@
-﻿namespace Calculator.Calculations
+﻿using Serilog;
+
+namespace Calculator.Calculations
 {
     public class Calculations
     {
-        public static int Sum(int a, int b) => a + b;
-        public static int Difference(int a, int b) => a - b;
+        public static int Sum(int a, int b)
+        {
+            int result = a + b;
+            Log.Information("Součet {A} + {B} = {Result}", a, b, result);
+            return result;
+        }
+        public static int Difference(int a, int b)
+        {
+            int result = a - b;
+            Log.Information("Rozdíl {A} - {B} = {Result}", a, b, result);
+            return result;
+        }
 
-        public static int Multiply(int a, int b) => a * b;
-        public static double Division(int a, int b) => (double)a / (double)b; // pro deleni s desetinnym vysledkem
+        public static int Multiply(int a, int b)
+        {
+            int result = a * b;
+            Log.Information("Součin {A} * {B} = {Result}", a, b, result);
+            return result;
+        }
+        public static double Division(int a, int b)
+        {
+            if (b == 0)
+            {
+                Log.Warning("Dělení nulou: {A} / {B}", a, b);
+                return double.NaN;
+            }
+
+            double result = (double)a / b;
+            Log.Information("Podíl {A} / {B} = {Result}", a, b, result);
+            return result;
+        }
 
     }
 }
