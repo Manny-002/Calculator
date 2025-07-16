e19caaf7bba80bc2e1a2a528837198072c59198f
Manny <honza.mencler@gmail.com>
2025-07-16 10:09:56 +0200

    Initial commit: Add sum and difference operations

--- Calculator.ConsoleApp/Program.cs
+++ Calculator.ConsoleApp/Program.cs
@@ -11,20 +11,15 @@ int b = Convert.ToInt32(Console.ReadLine());
 
 //vypis operaci
 
-//int soucet = a+b;
-//int rozdil = a - b;
-//int nasobeni = a * b;
-//int deleni = a / b; 
+int sum = a+b;
+int difference = a - b;
+
 
 
 //horsi zpusob
 
-//Console.WriteLine("Součet je:");
-//Console.WriteLine(soucet);
-//Console.WriteLine("Rozdíl je:");
-//Console.WriteLine(rozdil);
-//Console.WriteLine("Součin je:");
-//Console.WriteLine(nasobeni);
-//Console.WriteLine("Podíl je:"); 
-//Console.WriteLine(deleni);
+Console.WriteLine("Součet je:");
+Console.WriteLine(sum);
+Console.WriteLine("Rozdíl je:");
+Console.WriteLine(difference);
 
