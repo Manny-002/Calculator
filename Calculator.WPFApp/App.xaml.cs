951939b0190ac69bc9ed3c7a07b9beedb8766dea
Manny <honza.mencler@gmail.com>
2025-07-17 22:03:29 +0200

    🧮 Created WPF Calculator UI with MVVM architecture - Added Calculator.WPFApp project (WPF .NET) - Implemented MainWindow with XAML layout using Grid - Introduced MVVM structure: MainViewModel and RelayCommand - Added basic commands: NumberCommand, OperatorCommand, EqualCommand - Bound Display property and configured UI buttons - No code-behind in MainWindow.xaml.cs

---
+++ Calculator.WPFApp/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+using Calculator.WPFApp.ViewModels;
+
+namespace Calculator.WPFApp
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var mainWindow = new MainWindow
+            {
+                DataContext = new MainViewModel()
+            };
+
+            mainWindow.Show();
+        }
+    }
+}
---
+++ Calculator.WPFApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Calculator.WPFApp.ViewModels
+{
+    public class MainViewModel
+    {
+        public ICommand NumberCommand { get; }
+        public ICommand OperatorCommand { get; }
+        public ICommand EqualCommand { get; }
+
+        private string currentInput = "";
+        private string currentOperator = "";
+        private double firstNumber;
+
+        public string Display { get; set; } = "";
+
+        public MainViewModel()
+        {
+            NumberCommand = new RelayCommand(param => AppendNumber(param?.ToString()));
+            OperatorCommand = new RelayCommand(param => SetOperator(param?.ToString()));
+            EqualCommand = new RelayCommand(_ => Calculate());
+        }
+
+        private void AppendNumber(string number)
+        {
+            currentInput += number;
+            Display = currentInput;
+        }
+
+        private void SetOperator(string op)
+        {
+            if (double.TryParse(currentInput, out firstNumber))
+            {
+                currentInput = "";
+                currentOperator = op;
+            }
+        }
+
+        private void Calculate()
+        {
+            if (double.TryParse(currentInput, out double secondNumber))
+            {
+                double result = currentOperator switch
+                {
+                    "+" => firstNumber + secondNumber,
+                    "-" => firstNumber - secondNumber,
+                    "*" => firstNumber * secondNumber,
+                    "/" => secondNumber != 0 ? firstNumber / secondNumber : 0,
+                    _ => 0
+                };
+
+                Display = result.ToString();
+                currentInput = result.ToString();
+            }
+        }
+    }
+}
